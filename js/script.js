8a2779cfe96995715b7772a7868990207f17abd1
hodge-py <105604814+hodge-py@users.noreply.github.com>
2024-08-21 22:36:15 -0600

    c

--- js/script.js
+++ js/script.js
@@ -168,6 +168,22 @@ LinkedIn: https://www.linkedin.com/in/karson-hodge/
                         setTimeout(function() { $("input").focus(); }, 0);
                     });
                 }
+
+                else if(this.value !== ""){
+                    $(`<div>
+                        Error, Command not found, please type 'help' if unsure.<br>
+                         
+                        </div>
+                        <div class="cursor"><span style="color: chartreuse;padding-right: .0%;">TuxTerminal@KarsonHodge:~$</span>
+                    <input class="lineEnd" type="text" autofocus style="border: none; background-color: #232323; width: 75%;" /></div>
+                        `).insertAfter($(".cursor").last());
+
+                    var element = document.getElementById("terminal");
+                    element.scrollTop = element.scrollHeight;
+                    $("input").select().focus().blur(function() {
+                        setTimeout(function() { $("input").focus(); }, 0);
+                    });
+                }
                 
                 else{
                     $(`
