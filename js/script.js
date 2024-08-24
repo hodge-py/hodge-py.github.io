3a419552c7a0a3fa640269e3b992666db06ea051
hodge-py <105604814+hodge-py@users.noreply.github.com>
2024-08-23 18:19:56 -0600

    c

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
 
+    var configJson = ""
+
+    $.getJSON("config.json", function(json) {
+        configJson = json;
+        // this will show the info it in firebug console
+        $("#name2").text(`Created By ${configJson["secondName"]}`)
+        $("#barTerm").text(`TuxTerminal@${configJson["nameTop"]}: ~$`)
+    });
+
     $(document).on("keypress", ".lineEnd", function(e){
         var key = (e.keyCode || e.which);
         if(key == 13 || key == 3) {
@@ -42,12 +51,7 @@ $(document).ready(function(){
                         OTHER PORTFOLIO<br>
                         <a target="_blank" href="./gui">GUI Portfolio</a><br>
                         <pre>
- _  __                            _   _           _            
-| |/ /__ _ _ __ ___  ___  _ __   | | | | ___   __| | __ _  ___ 
-| ' // _\` | '__/ __|/ _ \\| '_ \\  | |_| |/ _ \\ / _\` |/ _\` |/ _ \\
-| . \\ (_| | |  \\__ \\ (_) | | | | |  _  | (_) | (_| | (_| |  __/
-|_|\\_\\__,_|_|  |___/\\___/|_| |_| |_| |_|\\___/ \\__,_|\\__, |\\___|
-                                                    |___/      
+ ${configJson["asciiName"]} 
                         </pre>
                          
                         </div>
@@ -65,53 +69,7 @@ $(document).ready(function(){
                 else if(this.value === "resume"){
                     $(`<div>
                         <pre>
-                        Karson Hodge
-Calgary, AB | 403-820-2405 | khodge1@hotmail.com | https://hodge-py.github.io/ 
-
-EDUCATION
-Southern Arkansas University\tAug 2023 – Present
-Master of Science in Computer and Information Science
-* Cumulative GPA: 3.8 / 4.0
-* Concentration in Data Science
-* Advanced Programming Concepts, Database Management Systems.
-
-University of Lethbridge\tSept 2016 – Apr 2021
-Bachelor of Science in Applied Statistics
-* Concentration in Economics\t
-\t
-WORK EXPERIENCE\t
-Eaton Corporation\tCalgary, AB
-Continuous Improvement Intern\tMay 2024 – Present
-* Assumed the role of product specialist along with all duties associated when filling in for a full-time employee on five weeks leave.
-* Handled the operations of the panel board line, maintaining a safe level of uptime to ensure production targets were met.
-* Redesigned inventory layout and implemented a new labelling system, reducing item retrieval time by ease of access and standardization.
-* Developed and implemented protocols for efficient data loading, minimizing errors and improving data reliability.
-
-Peoples Trust Company\tCalgary, AB
-Real Estate Analyst\tFeb 2022 – July 2023
-* Provided Underwriting for CMHC insured commercial loans to verify information, calculate risk, and insert profit spread.
-* Utilized excel to build cashflow models of pre-existing or construction-based properties, using financials and broker provided packages.
-* Verified new purchases of existing properties by either completing an internal assumption or CMHC approved replacement of covenant.
-* Created relationships with mortgage brokers when working on new deals, gathering documentation and networking at events.
-* Maintained strong lines of communication with colleagues to assure deals are vetted and processed correctly.
-
-Universal Rehabilitation Service Agency\tCalgary, AB
-Data Analyst and Quality Assurance\tNOV 2021 – Feb 2022
-* Developed and utilized databases with Microsoft Excel to improve quality control.
-* Modelled confidential data with accurate charts, metrics, and trends to evaluate programs impact.
-* Audited licenses and program files vetting for any legal mistakes or incorrect forms in compliance with accreditation agency standards.
-* Proposed new data collection strategies while maintaining great verbal and written communication with management to improve the development of documentation and policy.
-* Initiated a research project with Python and its statistical analysis modules to analyse program goals and develop dashboards with Power BI.
-
-PROJECTS\t
-Unknown-md.com\tFeb 2024 – Present
-* Markdown editor and render built with a HTML, CSS, JavaScript, and PHP Stack with the Bootstrap Framework and jQuery library.
-
-ADDITIONAL\t
-Languages: Python, Java, SQL, JavaScript, HTML, and CSS.
-Misc: Linux Commands, Pandas, NumPy, and Matplotlib.
-LinkedIn: https://www.linkedin.com/in/karson-hodge/ 
-
+                        ${configJson["plainResume"]}
                          </pre>
                         </div>
                         <div class="cursor"><span style="color: chartreuse;padding-right: .0%;">TuxTerminal@KarsonHodge:~$</span>
@@ -257,7 +215,7 @@ LinkedIn: https://www.linkedin.com/in/karson-hodge/
     const app = Vue.createApp({
         data() {
             return {
-                text: ''
+                text: '',
             }
         },
         methods: {
@@ -275,11 +233,6 @@ LinkedIn: https://www.linkedin.com/in/karson-hodge/
         setTimeout(function() { $("input").focus(); }, 0);
     });
 
-    var configJson = ""
-
-    $.getJSON("config.json", function(json) {
-        configJson = json; // this will show the info it in firebug console
-    });
 
 
 
